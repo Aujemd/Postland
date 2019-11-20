165c4e56d43f63566d3e8e5e345df1b2256a8d2a
Johan Mora <joenmodu99@gmail.com>
2019-11-20 01:29:33 -0400

    Modales de Modify y Delete implementados

--- resources/views/Posts/show.blade.php
+++ resources/views/Posts/show.blade.php
@@ -16,7 +16,76 @@
             <h5 class="card-title">{{$post->title}}</h5>
             <p class="card-text">{{$post->content}}</p>
             <p class="card-text"><small class="text-muted">Last Updated {{$post->updated_at}}</small></p>
+            
+            <div class="d-flex justify-content-around flex-wrap">
+              <!-- Button trigger modal -->
+              <button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#exampleModalLong">
+                Modify
+              </button>
+  
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title" id="exampleModalLongTitle">Modify Your Post</h1>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                    <form action="{{config('app.url')}}./posts/{{$post->id}}" method="POST">
+                        @csrf 
+                        @method('put')
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}">
+                        </div>
+                        <div class="form-group">
+                            <label for="content">Post</label>
+                            <textarea name="content" id="content" cols="30" rows="10"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="submit" class="btn btn-success">Save changes</button>
+                        </div>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <!-- Button trigger modal -->
+              <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#deletePostModal">
+                Delete
+              </button>
+  
+              <!-- Modal -->
+              <div class="modal fade" id="deletePostModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title bold" id="exampleModalLongTitle">¿Are you Sure?</h1>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <form action="{{config('app.url')}}./posts/{{$post->id}}" method="POST">
+                        @csrf 
+                        @method('delete')
+                        <p class=" text-muted italic">"This Post will be destroyed forever"</p>
+                        <div class="modal-footer">
+                          <button type="submit" class="btn btn-danger">Yes, I'm sure delete this</button>
+                        </div>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
+
         </div>
       </div>
     </div>
